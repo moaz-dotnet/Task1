ea5f74eabc462b1d58fedc2bae6e6d60e98c3eb3
Moaaz391 <madhmtwly5@gmail.com>
2025-10-04 02:20:31 +0300

    commit1 add some comment and Currency in code

--- arr/Program.cs
+++ arr/Program.cs
@@ -7,20 +7,23 @@ int pricePerLarge = 35;
  double taxRate = 0.06;
 Console.Write("Enter the number of small carpets: ");
 
-int smallCerpets = int.Parse(Console.ReadLine()  );
+int smallCarpets = int.Parse(Console.ReadLine()  );
 Console.Write("Enter the number of large carpets: ");
-int largeCerpets = int.Parse(Console.ReadLine()  );
+int largeCarpets = int.Parse(Console.ReadLine()  );
 Console.WriteLine("--------------------");
-int totalPrice = (smallCerpets * pricePerSmall) + (largeCerpets * pricePerLarge);
+//عملية حساب سعر غسل السجاد
+int totalPrice = (smallCarpets * pricePerSmall) + (largeCarpets * pricePerLarge);
+//عملية حساب الضريبة
 double tax = totalPrice * taxRate;
+//عملية حساب المبلغ الكلي
 double totalAmount = totalPrice + tax;
 
-Console.WriteLine($"the totalPrice is ${totalPrice}");
-Console.WriteLine($"the tax is ${tax}");
-Console.WriteLine($"the totalAmount is ${totalAmount}");
+Console.WriteLine($"the totalPrice is {totalPrice:C}");//C: currencyحبيت اجربها في تاسك هنا
+Console.WriteLine($"the tax is {tax}");
+Console.WriteLine($"the totalAmount is {totalAmount:C}");
 Console.WriteLine($"This estimate is valid for 30 days.");
 Console.WriteLine("--------------------");
-Console.WriteLine("Thank you for using our comany");
+Console.WriteLine("Thank you for using our company");
 
 
 
